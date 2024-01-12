8cc474c79dc8f451f97535c15af3add068421319
GaryYyq <yue3334069@icloud.com>
2024-01-12 17:15:21 +1000

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+
+function App() {
+  return (
+    <>
+    <Heading />
+    {notes.map(note => (<Note key={note.key} title={note.title} content={note.content}/>))}
+    <Footer />
+    
+    </>
+  );
+}
+
+export default App;
